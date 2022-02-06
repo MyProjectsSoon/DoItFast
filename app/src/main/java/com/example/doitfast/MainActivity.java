da756f7a71e31dab096e2ce2f10c41483e7bc114
MyProjectsSoon <81973845+MyProjectsSoon@users.noreply.github.com>
2022-02-06 22:25:12 +0400

    Declared attributes

--- app/src/main/java/com/example/doitfast/MainActivity.java
+++ app/src/main/java/com/example/doitfast/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.doitfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar=findViewById(R.id.progressBar);
     }
 }
--- app/src/main/java/com/example/doitfast/SignInActivity.java
+++ app/src/main/java/com/example/doitfast/SignInActivity.java
@@ -3,12 +3,33 @@ package com.example.doitfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
+    private CheckBox cbPassword;
+    private TextView txtSignUp;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        cbPassword = findViewById(R.id.cbPassword);
+        txtSignUp = findViewById(R.id.txtSignUp);
+
     }
 }
